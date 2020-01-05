b66f462fc05880578c58c6d6c797b002a929d777
Rikki Kendall <yrk3@duke.edu>
2020-01-05 02:41:30 -0500

    Update Footer.jsx

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,15 +2,14 @@ import React, { Component } from 'react';
 // Material UI Components
 import Typography from '@material-ui/core/Typography';
 import { Card } from '@material-ui/core/';
-import logo from './FFlogo.png';
 
 class Footer extends Component {
   render() {
     return (
       <footer className="App-header">
-        <img src= {logo}
+        {/* <img src= {}
         width="100"
-        height="120"/>
+        height="120"/> */}
         <Typography variant="h5"> Created by Rikki Kendall & <br /> Thea Dowrich </Typography>
         <br />
       </footer>
